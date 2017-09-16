188458bf996d68f42b59a1d7fa913f514b2e65c2
Jae-Holderby <jae.holderby@gmail.com>
2017-09-16 14:58:22 -0600

    server has access to heroku database

--- main.go
+++ main.go
@@ -14,10 +14,12 @@ type Student struct {
 	rfid     string
 	password string
 	partial  int
+	here     bool
+	excused  bool
 }
 
 func main() {
-	db, err := sql.Open("postgres", "https://secure-peak-53511.herokuapp.com")
+	db, err := sql.Open("postgres", "postgres://jwuzvvbakkswqj:638c8277583ccec74041d91560bd319728949fe1a8085eb0570ad9e7e29cbf3c@ec2-54-243-255-57.compute-1.amazonaws.com:5432/derfn0d9nq69b3")
 	if err != nil {
 		panic(err)
 	}
@@ -38,7 +40,7 @@ func main() {
 	studs := make([]Student, 0)
 	for rows.Next() {
 		stud := Student{}
-		err := rows.Scan(&stud.id, &stud.name, &stud.rfid, &stud.password, &stud.partial)
+		err := rows.Scan(&stud.id, &stud.name, &stud.rfid, &stud.password, &stud.partial, &stud.here, &stud.excused)
 		if err != nil {
 			panic(err)
 		}
@@ -50,6 +52,6 @@ func main() {
 
 	for _, stud := range studs {
 		// fmt.Println(bk.isbn, stud.title, bk.author, bk.price)
-		fmt.Printf("%d, %s, %s, %s, %b\n", stud.id, stud.name, stud.rfid, stud.password, stud.partial)
+		fmt.Printf("%d, %s, %s, %s, %b, %t, %t\n", stud.id, stud.name, stud.rfid, stud.password, stud.partial, stud.here, stud.excused)
 	}
 }
